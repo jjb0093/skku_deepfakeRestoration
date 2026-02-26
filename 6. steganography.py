79cd160a482f1417ed02c8c45b60ae4e906fb132
조준범 <jjb0093@naver.com>
2026-02-26 15:52:28 +0900

    Update 6. steganography.py

--- 6. steganography.py
+++ 6. steganography.py
@@ -1,6 +1,8 @@
 import pickle, cv2
 import numpy as np
+import ctypes as ct
 import jpegio as jio
+from pathlib import Path
 
 def expandBbox(bbox, img_h, img_w, margin = 0.1):
     x1, y1, x2, y2 = bbox
@@ -11,74 +13,90 @@ def expandBbox(bbox, img_h, img_w, margin = 0.1):
 
     return bboxNew
 
-def usableBlock(img_h, img_w, bbox, block = 8):
-    block_x = img_w // block
-    block_y = img_h // block
+def usableBlock(hc, wc, bbox, block = 8):
+    block_x = wc // block
+    block_y = hc // block
 
-    usable = np.ones((block_y, block_x), dtype = bool)
+    usable = np.ones((block_y, block_x), dtype = np.uint8)
     x1, y1, x2, y2 = bbox
 
     bx1, bx2 = x1 // block, (x2 - 1) // block
     by1, by2 = y1 // block, (y2 - 1) // block
 
+    #bx1, bx2 = max(0, bx1), min(block_x-1, bx2)
+    #by1, by2 = max(0, by1), min(block_y-1, by2)
+
+    '''
     usable[by1 : by2 + 1, bx1 : bx2 + 1] = False
     usableBlock = [(by, bx) for by in range(block_y) for bx in range(block_x) if usable[by, bx]]
+    '''
+    usable[by1:by2+1, bx1:bx2+1] = 0
 
-    return usableBlock
-
-def setParity(c, bit):
-    if(c == 0): return 0 if bit == 0 else 1
-    if(c&1 == bit): return c
-    return c + (1 if c>0 else -1)
-
-def embedding(image, code, usable, unusable, uvList = [(2, 3), (3, 2)]):
-    imgJpeg = jio.read(image)
-    coef = imgJpeg.coef_arrays[0]
-    length = len(code) * len(code[0]) // len(uvList)
-
-    h, w = coef.shape
-    print(f"가로 블럭 {w//8}개, 세로 블럭 {h//8}개, 총 {w*h//64}개")
-    print(f"사용해야 하는 블럭 수 : {length}")
-    print(f"사용가능 블럭 수 : {w*h//64 - unusable}")
-    
-    #blockNum = [0, 0]
-    #block = coef[8*blockNum[0] : 8*(blockNum[0]+1), 8*blockNum[1] : 8*(blockNum[1]+1)]
-
-    # 1. 특정 좌표부터 얼굴 부분을 제외하여 정사각형 그리기
-    margin = [50, 40]
-    usableCount, rowCount = 0, 0
-    for i in range(len(code)):
-        for j in range(w//8):
-            if((rowCount + margin[0], j + margin[1]) in usable):
-                block = coef[8*(i+margin[0]) : 8*(i+margin[0]+1), 8*(j+margin[1]) : 8*(j+margin[1]+1)]
-                block[0, 0] = int(block[0, 0]) + 200
-                #block[2, 3] = setParity(block[2, 3], code[usableCount // len(code[0])][usableCount % len(code[0])])
-                block[3, 2] = setParity(block[3, 2], code[usableCount // len(code[0])][usableCount % len(code[0])])
-                #for(u, v) in uvList:
-                    #block[u, v] = setParity(block[u][v], code[usableCount // len(code[0])][usableCount % len(code[0])])]
-                    #block[u, v] = 5
-                usableCount += 1
-
-            if(usableCount % len(code[0]) == 0):
-                rowCount += 1
-                break
-
-    path = "testData/testImageStegano.jpg"
-    jio.write(imgJpeg, path)
+    return np.ascontiguousarray(usable.reshape(-1), dtype = np.uint8)
 
 if(__name__ == "__main__"):
     with open("testData/testQRCode.pkl", 'rb') as f:
         code = pickle.load(f)
+        codeShape = len(code)
+        codeBit = code.reshape(-1).astype(np.uint8)
     with open("testData/testBbox.pkl", 'rb') as f:
         bbox = pickle.load(f)
     
-    imgPath = "testData/testImage.jpg"
-    img = cv2.imread(imgPath)
-    h, w = img.shape[:2]
+    imgInputPath = "testData/testImage.jpg"
+    imgOutputPath = imgInputPath.split('.')[0] + "_stegano." + imgInputPath.split('.')[-1]
 
-    bbox = expandBbox(bbox, h, w)
-    usable = usableBlock(h, w, bbox)
-    unusable = ((bbox[2]-bbox[0])//8) * ((bbox[3]-bbox[1])//8)
+    #img = cv2.imread(imgInputPath)
+    #h, w = img.shape[:2]
 
-    embedding(imgPath, code, usable, unusable)
-    
+    imgJpeg = jio.read(imgInputPath)
+    coef = imgJpeg.coef_arrays[0]
+    h, w = imgJpeg.image_height, imgJpeg.image_width
+    hc, wc = coef.shape
+
+    bbox = expandBbox(bbox, h, w, 0.3)
+    usable = usableBlock(hc, wc, bbox)
+    print(bbox)
+
+    #unusable = ((bbox[2]-bbox[0])//8) * ((bbox[3]-bbox[1])//8)
+
+    margin = [(bbox[2] - bbox[0]), 0]
+    print(margin)
+
+    print("coef.shape =", (hc, wc), "blocks =", (hc//8, wc//8), flush=True)
+    print("usable.len =", len(usable), "usable.sum =", int(usable.sum()), flush=True)
+    print("bits.len   =", len(codeBit), flush=True)
+
+    dll_path = Path(__file__).resolve().parent / "infoInsertion.dll"
+    dll = ct.CDLL(str(dll_path))
+
+    uvPair = ct.c_int * 2
+
+    dll.embedding.argtypes = [
+        ct.c_char_p, ct.c_char_p,
+        ct.POINTER(ct.c_ubyte), ct.c_size_t,
+        ct.POINTER(ct.c_ubyte),
+        ct.c_int, ct.c_int,
+        ct.POINTER(uvPair), ct.c_int,
+        ct.c_int, ct.c_int
+    ]
+    dll.embedding.restype = ct.c_int
+
+    uvList = (uvPair * 2)(uvPair(2, 3), uvPair(3, 2))
+    uvCount = ct.c_int(2)
+
+    bitsPtr = codeBit.ctypes.data_as(ct.POINTER(ct.c_ubyte))
+    usablePtr = usable.ctypes.data_as(ct.POINTER(ct.c_ubyte))
+
+    allumer = True
+
+    result = dll.embedding(
+        imgInputPath.encode("utf-8"),
+        imgOutputPath.encode("utf-8"),
+        bitsPtr, ct.c_size_t(len(codeBit)),
+        usablePtr,
+        ct.c_int(margin[0]), ct.c_int(margin[1]),
+        uvList, uvCount,
+        ct.c_int(codeShape), ct.c_int(1 if(allumer) else 0)
+    )
+
+    print("Embedding Result =", result)
