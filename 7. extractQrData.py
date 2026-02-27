ab143145dd3858fa46ab84b018a192d69c6c9313
조준범 <jjb0093@naver.com>
2026-02-27 21:21:04 +0900

    Update 7. extractQrData.py

--- 7. extractQrData.py
+++ 7. extractQrData.py
@@ -2,6 +2,8 @@ import jpegio as jio
 import numpy as np
 import cv2
 
+import pickle as pkl
+
 def extractDct(point, coef):
     hc, wc = coef.shape
     hy, hx = hc//8, wc//8
@@ -14,16 +16,20 @@ def extractDct(point, coef):
 
     return dct
 
-def dctImage(dct):
+def dctImage(dct, name):
     dct = dct * 255
     image = cv2.resize(dct, (dct.shape[1]*10, dct.shape[0]*10), interpolation = cv2.INTER_NEAREST)
-    cv2.imwrite("dctImg.png", image)
+    cv2.imwrite(f"testData/dctImg{name}.png", image)
 
-path = "testImage_stegano.jpg"
+path = "testData/testImage_stegano.jpg"
 
 img = jio.read(path)
 coef = img.coef_arrays[0]
 
-point = (1, 2)
-dct = extractDct(point, coef)
-dctImage(dct)
+point = [(1, 2), (2, 3)], ["12", "23"]
+for i in range(len(point[0])):
+    dct = extractDct(point[0][i], coef)
+    dctImage(dct, point[1][i])
+
+    with open(f"testData/imageDct{point[1][i]}.pkl", 'wb') as f:
+        pkl.dump(dct, f)
